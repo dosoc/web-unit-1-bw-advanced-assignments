4c992e0ea0393cefa60ba52a534bdb62e95d0a01
dosoc <18337634+dosoc@users.noreply.github.com~>
2022-04-24 17:45:34 -0500

    add interactive toggle switch

--- dark-theme/dark-theme.js
+++ dark-theme/dark-theme.js
@@ -1,16 +1,19 @@
 // you need to create DOM selectors to grab the correct HTML elements and update their styles to the opposite theme's css classes when you want to toggle the theme off and on
 const toggleLight = document.querySelector('.toggle-button')
 const title = document.querySelector('.title-light')
+const toggleSwitch = document.querySelector('.toggle-btn')
 
-toggleLight.addEventListener('mousedown', () => {
+
+toggleSwitch.addEventListener('click', ()=> {
     let mode = document.body.classList
     mode.toggle('dark');
+    toggleSwitch.classList.toggle('active')
     if (title.innerHTML === 'Light Mode'){
         title.innerHTML = 'Dark Mode'
-        toggleLight.innerHTML = 'Toggle Light Mode';
+
     } else if (title.innerHTML === 'Dark Mode' ) {
         title.innerHTML = 'Light Mode'
-        toggleLight.innerHTML = 'Toggle Dark Mode';
+
     }
-    
+   
 })
