cf60bea10fe4f4726906a50484c04c631c4d435e
dosoc <18337634+dosoc@users.noreply.github.com~>
2022-05-03 21:15:47 -0500

    add scroll bar HTML CSS JS

--- scroll-progress-bar/scroll-progress-bar.js
+++ scroll-progress-bar/scroll-progress-bar.js
@@ -1,3 +1,14 @@
 // most of the work here will be done with logic and a little math
 // what's the usual measurement of a progress bar? you'll need to measure the same way here
 // use that measurement to update the style of your inner fill element as you scroll
+const scrollBar = document.getElementById('scroll-fill')
+
+document.addEventListener('scroll', function(){
+    const scrolled = (scrollContainer().scrollTop /
+    (scrollContainer().scrollHeight - scrollContainer().clientHeight)) *100;
+
+    scrollBar.style.width = `${scrolled}%`
+})
+function scrollContainer(){
+    return document.documentElement || document.body;
+}
